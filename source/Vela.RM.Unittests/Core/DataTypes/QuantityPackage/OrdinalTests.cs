d8de2d21aaa6622a051d8515ba61e96e543b17f9
Nick Van Eeckhout <nickvane@users.noreply.github.com>
2011-02-20 14:46:36 +0100

    added unittests for AbsoluteQuantity and ReferenceRange, back at 100% coverage

---
+++ source/Vela.RM.Unittests/Core/DataTypes/QuantityPackage/OrdinalTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Vela.RM.Core.DataTypes.QuantityPackage;
+
+namespace Vela.RM.Unittests.Core.DataTypes.QuantityPackage
+{
+	[TestFixture]
+	public class WhenUsingOrdinal
+	{
+		[Test]
+		public void DefaultPropertiesShouldBeCorrect()
+		{
+			var ordinal1 = new Ordinal();
+			Assert.IsNotNull(ordinal1.GetLimits());
+			Ordinal ordinal2 = null;
+			var ordinal3 = new Ordinal();
+			Assert.IsFalse(ordinal1.IsStrictlyComparableTo(ordinal2));
+			Assert.IsTrue(ordinal1.IsStrictlyComparableTo(ordinal3));
+		}
+
+		[Test]
+		public void CompareShouldbeCorrect()
+		{
+			var ordinal1 = new Ordinal() {Value = 1};
+			var ordinal2 = new Ordinal() {Value = 2};
+			var ordinal3 = new Ordinal() {Value = 2};
+
+			Assert.AreEqual(0, ordinal2.CompareTo(ordinal3));
+			Assert.AreEqual(-1, ordinal1.CompareTo(ordinal2));
+			Assert.AreEqual(1, ordinal2.CompareTo(ordinal1));
+		}
+	}
+}
--- source/Vela.RM.Unittests/Core/DataTypes/QuantityPackage/ReferenceRangeTests.cs
+++ source/Vela.RM.Unittests/Core/DataTypes/QuantityPackage/ReferenceRangeTests.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using NUnit.Framework;
+using Vela.RM.Core.DataTypes.QuantityPackage;
 
 namespace Vela.RM.Unittests.Core.DataTypes.QuantityPackage
 {
-	class ReferenceRangeTests
+	[TestFixture]
+	public class WhenUsingReferenceRange
 	{
+		[Test]
+		public void DefaultPropertiesShouldBeCorrect()
+		{
+			var referenceRange = new ReferenceRange<OrderedTest>();
+			Assert.IsNotNull(referenceRange.Range);
+			var range = new Interval<OrderedTest>();
+			referenceRange.Range = range;
+			var orderedTest = new OrderedTest();
+			Assert.AreEqual(range.HasElement(orderedTest), referenceRange.IsInRange(orderedTest));
+		}
 	}
 }
