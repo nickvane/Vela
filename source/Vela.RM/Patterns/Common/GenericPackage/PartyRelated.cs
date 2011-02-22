91b208b27e5957584bc28260a05088dfc4ebc25d
Nick Van Eeckhout <nickvane@users.noreply.github.com>
2011-02-22 22:57:59 +0100

    Added lot of the classes from patterns.common

---
+++ source/Vela.RM/Patterns/Common/GenericPackage/PartyRelated.cs
@@ -0,0 +1,19 @@
+﻿using Vela.RM.Core.DataTypes.TextPackage;
+using Vela.RM.Core.Support;
+
+namespace Vela.RM.Patterns.Common.GenericPackage
+{
+	/// <summary>
+	/// Proxy type for identifying a party and its relationship to the subject of the record.
+	/// Use where the relationship between the party and the subject of the record must be known.
+	/// </summary>
+	[OpenEhrName("PARTY_RELATED")]
+	public class PartyRelated
+	{
+		/// <summary>
+		/// Relationship of subject of this ENTRY to the subject of the record. May be coded. If it is the patient, coded as “self”.
+		/// </summary>
+		[OpenEhrName("relationship")]
+		public CodedText Relationship { get; set; }
+	}
+}
---
+++ source/Vela.RM/Patterns/Common/GenericPackage/RevisionHistory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Vela.RM.Core.Support;
+
+namespace Vela.RM.Patterns.Common.GenericPackage
+{
+	///<summary>
+	/// Defines the notion of a revision history of audit items, each associated with theversion for which that audit was committed. The list is in most-recent-first order.
+	///</summary>
+	[OpenEhrName("REVISION_HISTORY")]
+	public class RevisionHistory
+	{
+		///<summary>
+		/// The items in this history in most-recent-last order.
+		///</summary>
+		[OpenEhrName("items")]
+		public IList<RevisionHistoryItem> Items { get; set; }
+
+		/// <summary>
+		/// The version id of the most recent item, as a String.
+		/// </summary>
+		/// <returns></returns>
+		[OpenEhrName("most_recent_version")]
+		public string GetMostRecentVersion()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// The commit date/time of the most recent item,  as a String.
+		/// </summary>
+		/// <returns></returns>
+		[OpenEhrName("most_recent_version_time_committed")]
+		public string GetCommittedTimeOfMostRecentVersion()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
