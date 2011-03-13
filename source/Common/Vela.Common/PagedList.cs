5618479165060009fd8e4a953dd56742708d128e
Nick Van Eeckhout <nickvane@users.noreply.github.com>
2011-03-13 15:22:04 +0100

    Added Vela.Portal projects

---
+++ source/Common/Vela.Common/PagedList.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace Ihc.Common
+{
+    /// <summary>
+    /// Result of one page and the total number of results 
+    /// </summary>
+    /// <typeparam name="T">Type of entity</typeparam>
+    // Class can be extended with ideas from these links:
+    // http://webcache.googleusercontent.com/search?q=cache:X4-R8B5ar5UJ:blogs.planetcloud.co.uk/mygreatdiscovery/post/Simple-paging-with-ASPNET-MVC-and-NHibernate.aspx+asp.net+nhibernate+paging&hl=en&gl=be&strip=1
+    // http://weblogs.asp.net/gunnarpeipman/archive/2010/09/14/returning-paged-results-from-repositories-using-pagedresult-lt-t-gt.aspx
+    // http://rookian.com/wordpressBlog/index.php/2010/09/25/paging-with-nhibernate-3-0-using-the-new-query-api-queryover/
+    // http://weblogs.asp.net/stefansedich/archive/2008/10/03/paging-with-nhibernate-using-a-custom-extension-method-to-make-it-easier.aspx
+    // http://blogs.planetcloud.co.uk/mygreatdiscovery/post/Simple-paging-with-ASPNET-MVC-and-NHibernate.aspx
+    public class PagedList<T>
+    {
+        ///<summary>
+        /// Create a new PagedList
+        ///</summary>
+        ///<param name="results">Results</param>
+        ///<param name="pageIndex">Index of the page</param>
+        ///<param name="pageSize">Size of each page</param>
+        ///<param name="totalItems">Total number of items</param>
+        public PagedList(IEnumerable<T> results, int pageIndex, int pageSize, int totalItems)
+        {
+            Results = results;
+            TotalItems = totalItems;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+        }
+
+        ///<summary>
+        /// Total of all items in the database
+        ///</summary>
+        public int TotalItems { get; private set; }
+
+        ///<summary>
+        /// Items for the requested page
+        ///</summary>
+        public IEnumerable<T> Results { get; private set; }
+
+        /// <summary>
+        /// Index of the requested page
+        /// </summary>
+        public int PageIndex { get; private set; }
+
+        /// <summary>
+        /// Size of each page
+        /// </summary>
+        public int PageSize { get; private set; }
+    }
+}
---
+++ source/Vela.Portal/Vela.Portal.Web/Plumbing/UnitOfWorkAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Castle.Windsor;
+using Raven.Client;
+
+namespace Vela.Portal.Web.Plumbing
+{
+	public class UnitOfWorkAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuted(ActionExecutedContext filterContext)
+		{
+			if (filterContext.Exception == null)
+			{
+				if (filterContext.HttpContext != null)
+				{
+					var container = ((IContainerAccessor)filterContext.HttpContext.ApplicationInstance).Container;
+					var session = container.Resolve<IDocumentSession>();
+					session.SaveChanges();
+				}
+			}
+
+			base.OnActionExecuted(filterContext);
+		}
+	}
+}
