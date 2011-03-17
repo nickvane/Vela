7a5be3fe0d003b9146e933cf45ddf8f4b0c93000
Nick Van Eeckhout <nickvane@users.noreply.github.com>
2011-03-17 23:03:01 +0100

    CompositionBuilder

--- source/Vela.VirtualEhr/Vela.SM.ArchetypeService/CompositionBuilder.cs
+++ source/Vela.VirtualEhr/Vela.SM.ArchetypeService/CompositionBuilder.cs
@@ -1,5 +1,8 @@
-﻿using Vela.AM.Aom.Archetypes;
+﻿using System;
+using Vela.AM.Aom.Archetypes;
+using Vela.RM.Core.Common.ArchetypedPackage;using Vela.RM.Core.Support.IdentificationPackage;
 using Vela.RM.Domain.Compositions.CompositionPackage;
+using Vela.RM.Domain.Compositions.CompositionPackage.ContentPackage;
 
 namespace Vela.SM.ArchetypeService
 {
@@ -9,7 +12,26 @@ namespace Vela.SM.ArchetypeService
 		{
 			var composition = new Composition();
 
+			composition.ArchetypeDetails = new Archetyped
+			                               	{
+			                               		ArchetypeId = new ArchetypeId(archetype.ArchetypeId.Value),
+			                               		Version = "1.0.2"
+			                               	};
+			composition.ArchetypeNodeId = archetype.ArchetypeId.Value;
+			composition.Content.Add(GetInstanceOfContentItem(archetype.Definition.ReferenceModelTypeName));
+			
 			return composition;
+		}
+
+		private ContentItem GetInstanceOfContentItem(string typeName)
+		{
+			object contentItem;
+			if (ReferenceModel.Classes.ContainsKey(typeName))
+			{
+				contentItem = Activator.CreateInstance(typeof(Locatable).Assembly.FullName, ReferenceModel.Classes[typeName].ClassName);
+
+			}
+			return null;
 		}
 	}
 }
